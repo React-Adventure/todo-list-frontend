68d4c96f74589b1a67b9a7978141e5bc096e2d84
DariaAzanovich <azanovich.dasha@gmail.com>
2022-07-07 15:25:06 +0300

    RA-51: ts + mobx config (failed)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/index.scss';
+
+if (!new class { x: Array<number>; }().hasOwnProperty('x')) throw new Error('Transpiler is not configured correctly');
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.common.js
+++ webpack.common.js
@@ -1,69 +1,84 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.tsx"),
+  entry: path.join(__dirname, 'src', 'index.tsx'),
   output: {
-    path: path.join(__dirname, "dist"), 
+    path: path.join(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-      favicon: path.join(__dirname, "public", "favicon.ico"),
+      template: path.join(__dirname, 'public', 'index.html'),
+      favicon: path.join(__dirname, 'public', 'favicon.ico'),
     }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/, 
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
             plugins: [
               ['@babel/plugin-transform-runtime', {
-                "regenerator": true
-              }]
-            ]
-          }
+                regenerator: true,
+              }],
+            ],
+          },
         },
       },
       {
         test: /\.(ts|tsx)$/,
+        resolve: {
+          extensions: ['.tsx', '.ts', '.js'],
+        },
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
           },
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env', '@babel/preset-react', "@babel/preset-typescript"],
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
               plugins: [
-                ['@babel/plugin-transform-runtime', {
-                  "regenerator": true
-                }], "@babel/plugin-proposal-class-properties"
-              ]
-            }
-          }
-      ],
+                // ['@babel/plugin-transform-flow-strip-types'],
+                ['@babel/plugin-transform-runtime'/*, {
+                  regenerator: true,
+                }*/],
+                // ['@babel/plugin-proposal-decorators', {
+                //   legacy: true,
+                // }],
+                ['@babel/plugin-proposal-class-properties', { loose: false }],
+              ],
+              assumptions: {
+                setPublicClassFields: true,
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader", 
-          "css-loader",
-          "sass-loader",
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: "url-loader",
+        loader: 'url-loader',
         options: { limit: false },
       },
     ],
   },
+  // externals: {
+  //   "react": "React",
+  //   "react-dom": "ReactDOM"
+  // },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },

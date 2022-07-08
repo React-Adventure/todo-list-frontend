25db89fce112e7d8b848c6e8920d06add34106d1
DariaAzanovich <azanovich.dasha@gmail.com>
2022-07-08 15:14:58 +0300

    Ra-51: del useless code

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,3 @@ import './styles/index.scss';
 const root = createRoot(document.getElementById('root'));
 
 root.render(<App />);
-
-// eslint-disable-next-line max-len
-if (!new class { x: Array<number>; }().hasOwnProperty('x')) throw new Error('Transpiler is not configured correctly');
